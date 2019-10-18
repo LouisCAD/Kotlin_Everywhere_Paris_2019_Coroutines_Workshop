b3147c527e4c072f3cee51c3f10136b473eb1202
Louis CAD <louis.cognault@gmail.com>
2019-10-18 14:57:35 +0200

    Add withEnterAndExitLog utility function

---
+++ exercises/src/main/kotlin/util/Logging.kt
@@ -0,0 +1,22 @@
+package util
+
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+import kotlin.time.ExperimentalTime
+import kotlin.time.MonoClock
+
+@UseExperimental(ExperimentalContracts::class, ExperimentalTime::class)
+inline fun <R> withEnterAndExitLog(name: String, block: () -> R): R {
+    contract { callsInPlace(block, InvocationKind.EXACTLY_ONCE) }
+    println("$name: called")
+    val startClockMark = MonoClock.markNow()
+    try {
+        val returnValue = block()
+        println("$name: returned in ${startClockMark.elapsedNow().toLongMilliseconds()}ms")
+        return returnValue
+    } catch (t: Throwable) {
+        println("$name threw after ${startClockMark.elapsedNow().toLongMilliseconds()}ms")
+        throw t
+    }
+}
